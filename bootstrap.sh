256357265a658636d7f13d1d4f2c9d330576a842
Joe McTee <mcjoe@jeklsoft.com>
2015-12-29 10:46:14 -0700

    Created bootstrap script and added README. TODO list keeps getting longer.

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+git clone git@github.com:jmctee/ProjectStarter.git
+mv ProjectStarter $1
+cd $1
+
+rm -Rf .git TODO
+
+git init
+git add .gitignore build.gradle src
+git commit -am "${1}: bootstrapped from https://github.com/jmctee/ProjectStarter"
+git status
+git log
+
+gradle cleanIdea
+gradle idea
+gradle build
+
+echo "Project ${1} now ready for use, open in IntelliJ Idea and got to town!"
